2453e7bbed9bb88f66d1d1b56646983e50201672
up202004187 <up202004187@up.pt>
2023-09-29 01:00:47 +0100

    Allow cake to rotate missing slice position

--- tp1/MyObjects/MyCake.js
+++ tp1/MyObjects/MyCake.js
@@ -12,17 +12,19 @@ class MyCake extends THREE.Object3D {
      * @param {number} radius the radius of the cake. Default is `0.8`
      * @param {number} height the height of the cake. Default is `radius/2`
      * @param {number} sliceSize the size (radians) of the missing slice. Default is `PI/4`
+     * @param {number} slicePosition the radial rotation (radians) of the missing slice, around the Y axis. Default is `0`
      * @param {number} diffuseCakeColor the diffuse component of the cake's color. Default `#eb8a52`
      * @param {number} specularCakeColor the specular component of the cake's color. Default `#777777`
      * @param {number} cakeShininess the shininess component of the cake's color. Default `30` 
      */
-    constructor(app, radius, height, sliceSize, diffuseCakeColor, specularCakeColor, cakeShininess) {
+    constructor(app, radius, height, sliceSize, slicePosition, diffuseCakeColor, specularCakeColor, cakeShininess) {
         super();
         this.app = app;
         this.type = 'Group';
         this.radius = radius || 0.8
         this.height = height || this.radius/2
         this.sliceSize = sliceSize || Math.PI/4
+        this.slicePosition = slicePosition || Math.PI
         this.diffuseCakeColor = diffuseCakeColor || "#eb8a52"
         this.specularCakeColor = specularCakeColor || "#777777"
         this.cakeShininess = cakeShininess || 30
@@ -30,7 +32,7 @@ class MyCake extends THREE.Object3D {
         this.cakeMaterial = new THREE.MeshPhongMaterial({ color: this.diffuseCakeColor, 
             specular: this.specularCakeColor, emissive: "#000000", shininess: this.cakeShininess })
 
-        let cake = new THREE.CylinderGeometry(this.radius, this.radius, this.height, 32, 1, false, 0, (2*Math.PI - this.sliceSize))
+        let cake = new THREE.CylinderGeometry(this.radius, this.radius, this.height, 32, 1, false, this.slicePosition, (2*Math.PI - this.sliceSize))
         let cake_inside = new THREE.PlaneGeometry(this.radius, this.height)
         
         // cake
@@ -39,13 +41,13 @@ class MyCake extends THREE.Object3D {
 
         // left inside part
         this.leftInsideMesh = new THREE.Mesh(cake_inside, this.cakeMaterial)
-        this.leftInsideMesh.rotateY(this.sliceSize)
+        this.leftInsideMesh.rotateY(this.sliceSize+this.slicePosition)
         this.leftInsideMesh.translateX(-this.radius/2)
         this.add( this.leftInsideMesh )
 
         // right inside part
         this.rightInsideMesh = new THREE.Mesh(cake_inside, this.cakeMaterial)
-        this.rightInsideMesh.rotateY(-Math.PI/2)
+        this.rightInsideMesh.rotateY(-(Math.PI/2 + this.slicePosition))
         this.rightInsideMesh.translateX(this.radius/2)
         this.add( this.rightInsideMesh )
         
