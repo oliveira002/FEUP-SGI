67b91077d7241bc0243374d6810e0f0aa415e868
oliveira002 <deadzin112@gmail.com>
2023-10-05 20:10:18 +0100

    Fix Furniture

--- tp1/MyObjects/MyFurniture.js
+++ tp1/MyObjects/MyFurniture.js
@@ -43,59 +43,31 @@ class MyFurniture extends THREE.Object3D {
         
         let top = new THREE.BoxGeometry(this.furLength, this.furHeight * 0.05, this.furDepth)
         let bot = new THREE.BoxGeometry(this.furLength, this.furHeight * 0.05, this.furDepth)
-        let left = new THREE.BoxGeometry(this.furHeight  * 0.05, this.furHeight, this.furDepth)
-        let right = new THREE.BoxGeometry(this.furHeight  * 0.05, this.furHeight, this.furDepth)
-        let back = new THREE.BoxGeometry(this.furLength , this.furHeight * 0.05, this.furDepth)
-        let frontLeft = new THREE.BoxGeometry(this.furLength * 0.25, this.furHeight * 0.05, this.furDepth)
-        let frontRight = new THREE.BoxGeometry(this.furLength * 0.25, this.furHeight * 0.05, this.furDepth)
+        let box = new THREE.BoxGeometry(this.furLength * 0.25, this.furHeight * 0.90, this.furDepth)
         let drawer = new THREE.BoxGeometry(this.furLength * 0.5, this.furHeight * 0.05, this.furDepth * 0.5)
         let inside = new THREE.BoxGeometry(this.furLength * 0.5, this.furHeight * 0.05, this.furDepth)
-        let insideSide = new THREE.BoxGeometry(this.furHeight  * 0.05, this.furHeight, this.furDepth)
 
         this.topFur = new THREE.Mesh(top, this.furMaterial)
-        this.topFur.translateY(this.furHeight)
+        this.topFur.translateY(this.furHeight * 0.95)
         this.add( this.topFur );
 
         this.botFur = new THREE.Mesh(bot, this.furMaterial)
         this.botFur.translateY(this.furHeight * 0.05 / 2)
         this.add( this.botFur );
 
-        this.leftFur = new THREE.Mesh(left, this.furMaterial)
-        this.leftFur.rotateX(Math.PI / 2)
-        this.leftFur.translateZ(-this.furHeight / 2)
-        this.leftFur.translateX(-this.furLength / 2 + this.furHeight * 0.025)
-        this.add( this.leftFur );
+        this.boxRightMesh = new THREE.Mesh(box, this.furMaterial)
+        this.boxRightMesh.translateY(this.furHeight * 0.5)
+        this.boxRightMesh.translateX(this.furLength * 0.375)
+        this.add( this.boxRightMesh );
 
-        this.rightFur = new THREE.Mesh(right, this.furMaterial)
-        this.rightFur.rotateX(Math.PI / 2)
-        this.rightFur.translateZ(-this.furHeight / 2)
-        this.rightFur.translateX(this.furLength / 2 - this.furHeight * 0.025)
-        this.add( this.rightFur );
-
-        
-        this.backFur = new THREE.Mesh(back, this.furMaterial2)
-        this.backFur.translateZ(-this.furHeight / 2 + this.furHeight  * 0.05 / 2)
-        this.backFur.translateY(this.furDepth / 2)
-        this.backFur.rotateX(Math.PI / 2)
-        this.add( this.backFur );
-        
-        this.frontLeftFur = new THREE.Mesh(frontLeft, this.furMaterial2)
-        this.frontLeftFur.translateZ(this.furHeight / 2 - this.furHeight  * 0.05 / 2)
-        this.frontLeftFur.translateY(this.furDepth / 2)
-        this.frontLeftFur.translateX(-this.furLength / 2 + 0.125 * this.furLength)
-        this.frontLeftFur.rotateX(Math.PI / 2)
-        this.add(this.frontLeftFur);
-
-        this.frontRightFur = new THREE.Mesh(frontRight, this.furMaterial2)
-        this.frontRightFur.translateZ(this.furHeight / 2 - this.furHeight  * 0.05 / 2)
-        this.frontRightFur.translateY(this.furDepth / 2)
-        this.frontRightFur.translateX(this.furLength / 2 - 0.125 * this.furLength)
-        this.frontRightFur.rotateX(Math.PI / 2)
-        this.add(this.frontRightFur);
+        this.boxLeftMesh = new THREE.Mesh(box, this.furMaterial)
+        this.boxLeftMesh.translateY(this.furHeight * 0.5)
+        this.boxLeftMesh.translateX(-this.furLength * 0.375)
+        this.add( this.boxLeftMesh );
 
         this.frontDrawer = new THREE.Mesh(drawer, this.furMaterial2)
         this.frontDrawer.translateZ(this.furHeight / 2 - this.furHeight  * 0.05 / 2)
-        this.frontDrawer.translateY(this.furDepth / 4)
+        this.frontDrawer.translateY(this.furDepth / 4 + (this.furHeight * 0.05 / 2))
         this.frontDrawer.rotateX(Math.PI / 2)
         this.add(this.frontDrawer);
 
@@ -103,18 +75,7 @@ class MyFurniture extends THREE.Object3D {
         this.insideDrawer.translateY(this.furHeight * 0.5)
         this.add( this.insideDrawer );
 
-        this.insideLeft = new THREE.Mesh(insideSide, this.furMaterial)
-        this.insideLeft.translateY(this.furHeight / 2)
-        this.insideLeft.translateX(this.furLength / 4)
-        this.insideLeft.rotateX(Math.PI / 2)
-        this.add( this.insideLeft );
 
-        this.insideRight = new THREE.Mesh(insideSide, this.furMaterial)
-        this.insideRight.translateY(this.furHeight / 2)
-        this.insideRight.translateX(-this.furLength / 4)
-        this.insideRight.rotateX(Math.PI / 2)
-        this.add( this.insideRight );
-        
         this.translateY(-this.furnitureHeight * 0.5)
         this.scale.set(1,this.furnitureHeight,this.furnitureDepth)
     }
