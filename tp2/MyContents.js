6815bb58cf4e3a4c3e671b638991c3374350667d
oliveira002 <deadzin112@gmail.com>
2023-11-16 23:36:19 +0000

    Starting the projectgit add -Agit add -A

--- tp2/MyContents.js
+++ tp2/MyContents.js
@@ -366,6 +366,25 @@ class MyContents  {
 
                 return mesh
             }
+            case "sphere": {
+                let metrics = node.representations[0]
+
+                let prim = new THREE.SphereGeometry(
+                    metrics.radius,
+                    metrics.slices,
+                    metrics.stacks,
+                    metrics.phistart,
+                    metrics.philength,
+                    metrica.thetastart,
+                    metrics.thetalength
+                )
+                
+                let mesh = new THREE.Mesh(prim, mat)
+                mesh.receiveShadow = true
+                mesh.castShadow = true
+
+                return mesh
+            }
         }
     }
 
