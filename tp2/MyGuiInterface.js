c5aad26eddb43216b05076c5e9a502ad9221754b
oliveira002 <deadzin112@gmail.com>
2023-11-21 19:13:00 +0000

    wireframe

--- tp2/MyGuiInterface.js
+++ tp2/MyGuiInterface.js
@@ -15,6 +15,7 @@ class MyGuiInterface  {
         this.app = app
         this.datgui =  new GUI();
         this.contents = null
+        this.wireframeEnabled = false
     }
 
     /**
@@ -22,22 +23,30 @@ class MyGuiInterface  {
      * @param {MyContents} contents the contents objects 
      */
     setContents(contents) {
-        this.contents = contents
-    }
-
-    /**
-     * Initialize the gui interface
-     */
+        this.contents = contents;
+      }
+    
     init() {
-
-        // check if there are cameras
-        console.log(this.app.cameras)
-        if(Array.isArray(this.app.cameras) && Object.keys(this.app.cameras).length){
-            const cameraFolder = this.datgui.addFolder('Camera')
+        if (Array.isArray(this.app.cameras) && Object.keys(this.app.cameras).length) {
+            const cameraFolder = this.datgui.addFolder('Camera');
             cameraFolder.add(this.app, 'activeCameraName', Object.keys(this.app.cameras)).name("Active camera");
-            cameraFolder.open()
+            cameraFolder.open();
         }
+
+        this.datgui.add(this, 'wireframeEnabled').name('Toggle Wireframe').onChange(() => {
+
+            let materialIds = Object.keys(this.contents.materialMap);
+
+            for (const materialId of materialIds) {
+                const material = this.contents.materialMap[materialId];
+                if (material) {
+                    material.wireframe = this.wireframeEnabled;
+                } 
+            }
+        });
+
     }
 }
+    
 
 export { MyGuiInterface };
