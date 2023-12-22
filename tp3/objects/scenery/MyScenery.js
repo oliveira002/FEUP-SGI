9ff909634b74ef261f31c356015069bcd62ccb51
oliveira002 <deadzin112@gmail.com>
2023-12-22 15:30:50 +0000

    fix shaders

--- tp3/objects/scenery/MyScenery.js
+++ tp3/objects/scenery/MyScenery.js
@@ -11,17 +11,22 @@ class MyScenery extends THREE.Object3D{
 
 		this.geometry = new THREE.PlaneGeometry(10, 10,50,50);
 		
-		this.heightMapTex = new THREE.TextureLoader().load('../../images/heightmap.jpg' )
+		this.heightMapTex = new THREE.TextureLoader().load('../../images/heightmap2.jpg' )
 		this.heightMapTex.wrapS = THREE.RepeatWrapping;
 		this.heightMapTex.wrapT = THREE.RepeatWrapping;
 
-        this.terrainTex = new THREE.TextureLoader().load('../../images/terrain.jpg' )
+        this.terrainTex = new THREE.TextureLoader().load('../../images/terrain2.jpg' )
 		this.terrainTex.wrapS = THREE.RepeatWrapping;
 		this.terrainTex.wrapT = THREE.RepeatWrapping;
 
+		this.altimetry = new THREE.TextureLoader().load('../../images/altimetry.png' )
+		this.altimetry.wrapS = THREE.RepeatWrapping;
+		this.altimetry.wrapT = THREE.RepeatWrapping;
+
 		this.shader = new MyShader(this.app, 'Terrain Shader', "Terrain Shader", "../../shaders/terrain.vert", "../../shaders/terrain.frag", {
-			uSampler1: {type: 'sampler2D', value: this.heightMapTex },
+			uSampler1: {type: 'sampler2D', value: this.heightMapTex},
 			uSampler2: {type: 'sampler2D', value: this.terrainTex },
+			uSampler3: {type: 'sampler2D', value: this.altimetry },
 			normScale: {type: 'f', value: 2.0 },
 			displacement: {type: 'f', value: 0.0 },
 			normalizationFactor: {type: 'f', value: 1 },
