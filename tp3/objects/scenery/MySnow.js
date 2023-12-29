61823b5b289d38e0600ebce4113b859f121376cd
oliveira002 <deadzin112@gmail.com>
2023-12-29 15:17:41 +0000

    better snow?

--- tp3/objects/scenery/MySnow.js
+++ tp3/objects/scenery/MySnow.js
@@ -1,95 +1,92 @@
-import * as THREE from 'three'
+import * as THREE from 'three';
 
 class MySnow extends THREE.Object3D {
+  constructor(app, scene) {
+    super();
+    this.app = app;
+    this.scene = scene;
+    this.snowList = [];
+    this.angle = 0;
 
-    constructor(app, scene) {
-        super()
-        this.app = app
-        this.scene = scene
-        this.snowList = [];
-        this.angle = 0;
+    var length = 100; // Increase the number of particles
+    var planeSize = 175;
 
-        var length = 10;
+    var geometry = new THREE.BufferGeometry();
 
-        var geometry = new THREE.BufferGeometry();
+    var materials = [];
 
-        var materials = [];
+    var textureLoader = new THREE.TextureLoader();
+    var sprite1 = textureLoader.load('https://dl.dropbox.com/s/13ec3ht27adnu1l/snowflake1.png?dl=0');
+    var sprite2 = textureLoader.load('https://dl.dropbox.com/s/rczse8o8zt5mxe6/snowflake2.png?dl=0');
+    var sprite3 = textureLoader.load('https://dl.dropbox.com/s/cs17pph4bu096k7/snowflake3.png?dl=0');
+    var sprite4 = textureLoader.load('https://dl.dropbox.com/s/plwtcfvokuoz931/snowflake4.png?dl=0');
+    var sprite5 = textureLoader.load('https://dl.dropbox.com/s/uhh77omqdwqo2z5/snowflake5.png?dl=0');
 
-        var textureLoader = new THREE.TextureLoader();
-        var sprite1 = textureLoader.load('https://dl.dropbox.com/s/13ec3ht27adnu1l/snowflake1.png?dl=0');
-        var sprite2 = textureLoader.load('https://dl.dropbox.com/s/rczse8o8zt5mxe6/snowflake2.png?dl=0');
-        var sprite3 = textureLoader.load('https://dl.dropbox.com/s/cs17pph4bu096k7/snowflake3.png?dl=0');
-        var sprite4 = textureLoader.load('https://dl.dropbox.com/s/plwtcfvokuoz931/snowflake4.png?dl=0');
-        var sprite5 = textureLoader.load('https://dl.dropbox.com/s/uhh77omqdwqo2z5/snowflake5.png?dl=0');
-
-        var vertices = [];
-        for ( var i = 0; i < length; i ++ ) {
-            var x = this.getRandom(-500, 500);
-            var y = this.getRandom(300, 400);
-            var z = this.getRandom(-500, 500);
-            vertices.push( x, y, z );
-        }
-
-        geometry.setAttribute( 'position', new THREE.Float32BufferAttribute( vertices, 3 ) );
-
-        var parameters = [
-            ["#FFFFFF", sprite2, this.getRandom(10, 10) ],
-            ["#FFFFFF", sprite3, this.getRandom(10, 15) ],
-            ["#FFFFFF", sprite1, this.getRandom(10, 15) ],
-            ["#FFFFFF", sprite5, this.getRandom(5, 10) ],
-            ["#FFFFFF", sprite4, this.getRandom(5, 10) ]
-        ];
-
-        for ( var i = 0; i < parameters.length; i ++ ) {
-            var sprite = parameters[ i ][ 1 ];
-            var size = parameters[ i ][ 2 ];
-            materials[ i ] = new THREE.PointsMaterial({
-                size: size,
-                map: sprite,
-                blending: THREE.AdditiveBlending,
-                depthTest: true,
-                transparent: true
-            } );
-
-            var particles = new THREE.Points( geometry, materials[ i ] );
-            particles.rotation.x = Math.random() * 360;
-            particles.rotation.y = Math.random() * 360;
-            particles.rotation.z = Math.random() * 360;
-            particles.vx = 0;
-            particles.vy = 0;
-            particles.material.opacity = 0;
-
-            this.add(particles);
-            this.snowList.push(particles);
-        }
+    var vertices = [];
+    for (var i = 0; i < length; i++) {
+      var x = this.getRandom(-planeSize / 2, planeSize / 2);
+      var y = this.getRandom(175, 200);
+      var z = this.getRandom(-planeSize / 2, planeSize / 2);
+      vertices.push(x, y, z);
     }
 
-    update() {
-        this.angle += 0.001;
-    
-        for (var i = 0; i < this.snowList.length; i++) {
-          this.snowList[i].material.opacity += 0.01;
-          this.snowList[i].vy -= 1;
-          this.snowList[i].vx = Math.sin(this.angle)*Math.cos(this.angle)*10;
-    
-          this.snowList[i].vx *= 0.2;
-          this.snowList[i].vy *= 0.6;
-    
-          this.snowList[i].position.x += this.snowList[i].vx;
-          this.snowList[i].position.y += this.snowList[i].vy;
-    
-          if (this.snowList[i].position.y < -1000) {
-            this.snowList[i].material.opacity += 0.1;
-            this.remove(this.snowList[i]);
-            this.snowList.splice(i, 1);
-            i -= 1;
-          }
-        }
-    }
+    geometry.setAttribute('position', new THREE.Float32BufferAttribute(vertices, 3));
 
-    getRandom(min, max) {
-        return Math.random() * (max - min) + min;
+    var parameters = [
+      ['#FFFFFF', sprite2, this.getRandom(0.1, 0.3)],
+      ['#FFFFFF', sprite3, this.getRandom(0.1, 0.3)],
+      ['#FFFFFF', sprite1, this.getRandom(0.1, 0.3)],
+      ['#FFFFFF', sprite5, this.getRandom(0.1, 0.3)],
+      ['#FFFFFF', sprite4, this.getRandom(0.1, 0.3)],
+    ];
+
+    for (var i = 0; i < parameters.length; i++) {
+      var sprite = parameters[i][1];
+      var size = parameters[i][2];
+      materials[i] = new THREE.PointsMaterial({
+        size: size,
+        map: sprite,
+        blending: THREE.AdditiveBlending,
+        depthTest: true,
+        transparent: true,
+      });
+
+      var particles = new THREE.Points(geometry, materials[i]);
+      particles.rotation.x = Math.random() * 360;
+      particles.rotation.y = Math.random() * 360;
+      particles.rotation.z = Math.random() * 360;
+      particles.vx = 0;
+      particles.vy = 0;
+      particles.material.opacity = 0;
+
+      this.add(particles);
+      this.snowList.push(particles);
     }
+  }
+
+  update() {
+    this.angle += 0.001;
+
+    for (var i = 0; i < this.snowList.length; i++) {
+      this.snowList[i].material.opacity += 0.01;
+      this.snowList[i].vy -= 0.5;
+
+      this.snowList[i].vy *= 0.2;
+
+      this.snowList[i].position.y += this.snowList[i].vy;
+
+      if (this.snowList[i].position.y < -1000) {
+        this.snowList[i].material.opacity += 0.1;
+        this.remove(this.snowList[i]);
+        this.snowList.splice(i, 1);
+        i -= 1;
+      }
+    }
+  }
+
+  getRandom(min, max) {
+    return Math.random() * (max - min) + min;
+  }
 }
 
-export { MySnow }
+export { MySnow };
